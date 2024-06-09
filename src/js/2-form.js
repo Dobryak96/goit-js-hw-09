d1b6a28e8607aca311db7fd08ee3ae390ae87186
Dobryak96 <gorokhov_olexandr@ukr.net>
2024-06-09 21:18:21 +0100

    new

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -2,7 +2,7 @@ let formData = {
     email: "",
     message: "",
 };
-// Знаходимо елементи та додаємо класи для стилізаціі
+
 const labels = document.querySelectorAll('label');
 labels.forEach(label => { label.classList.add('form-label') });
 const input = document.querySelector('input');
@@ -12,7 +12,6 @@ text.classList.add('form-message');
 const button = document.querySelector('button');
 button.classList.add('form-btn');
 
-// Створюємо функціі додавання даних у локальне сховище та їх читання
 function toLocalStorage() {
     localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 }
@@ -26,17 +25,14 @@ function fromLocalStorage() {
     }
 }
 
-// Додаємо слухача події input для відстеження змін у формі та їх запису у сховище
 const form = document.querySelector('.feedback-form');
 form.addEventListener('input', (e) => {
     formData[e.target.name] = e.target.value.trim();
     toLocalStorage();
 });
 
-// При завантаженні сторінки перевіряємо чи є дані у локальному сховищі та використовємо їх для заповнення форми
 window.addEventListener('DOMContentLoaded', fromLocalStorage);
 
-// Додаємо слухача події submit та обробляємо належним чином
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     if (formData.email === "" || formData.message === "") {
